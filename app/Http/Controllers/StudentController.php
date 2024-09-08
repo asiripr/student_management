0f2a6291af7d81078c39256b5ede07fa2d736996
asiripr <asiripramodaya2000@gmail.com>
2024-09-08 14:04:30 +0530

    created index front end page

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -11,7 +12,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::all();
+        return view('students.index')->with('students',$students);
     }
 
     /**
