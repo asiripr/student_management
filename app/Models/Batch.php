c29d639875f5d0798d1cdb3ba802f4ca5cf336c7
asiripr <asiripramodaya2000@gmail.com>
2024-09-29 15:56:20 +0530

    fixed the issue with batches

--- app/Models/Batch.php
+++ app/Models/Batch.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Batch extends Model
 {
     protected $table = 'batches';
-    protected $primarykey = 'id';
-    protected $filltable = ['name','course_id','start_date'];
+    protected $primaryKey = 'id';
+    protected $fillable = ['name','course_id','start_date'];
 
     use HasFactory;
 }
--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -8,11 +8,11 @@
       <form action="{{ url('batches') }}" method="post">
         {!! csrf_field() !!}
         <label>Batch Name</label></br>
-        <input type="text" name="batchname" id="batchname" class="form-control"></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
         <label>Course Name</label></br>
-        <input type="text" name="coursename" id="coursename" class="form-control"></br>
+        <input type="text" name="course_id" id="course_id" class="form-control"></br>
         <label>Start Date</label></br>
-        <input type="text" name="startdate" id="startdate" class="form-control"></br>
+        <input type="text" name="start_date" id="start_date" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
