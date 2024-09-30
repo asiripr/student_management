6455e20af8d14463e95bfd5bb652b762820e970a
asiripr <asiripramodaya2000@gmail.com>
2024-09-30 08:14:38 +0530

    enrollment controller and its model

---
+++ app/Http/Controllers/EnrollmentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Enrollment;
+use Illuminate\Http\Request;
+
+class EnrollmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $enrollments = Enrollment::all();
+        return view('enrollments.index')->with('enrollemnts',$enrollments);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('enrollments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        Enrollment::create($input);
+        return redirect('enrollments')->with('flash_message','Enrollemnt added!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $enrollement = Enrollment::find($id);
+        return view('enrollments.show')->with('enrollement',$enrollement);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $enrollement = Enrollment::find($id);
+        return view('enrollments.edit')->with('enrollement',$enrollement);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $enrollement = Enrollment::find($id);
+        $input = $request->all();
+        $enrollement->update($input);
+        return view('enrollments')->with('flash_message','Enrollment updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Enrollment::destroy($id);
+        return redirect('enrollment')-with('flash_message','seccessfully deleted');
+    }
+}
---
+++ app/Models/Enrollment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enrollment extends Model
+{
+    protected $table = 'enrollments';
+    protected $primarykey = 'id';
+    protected $fillable = ['name','enroll_no','batch_id','student_id','join_date','fee'];
+
+    use HasFactory;
+}
