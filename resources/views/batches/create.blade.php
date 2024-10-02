0eaaa5a1e6d3f203618f9aff202462ca07e4b1f3
asiripr <asiripramodaya2000@gmail.com>
2024-10-02 05:40:33 +0530

    batches with dropdown menu

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -1,22 +1,29 @@
 @extends('layout')
 @section('content')
- 
-<div class="card">
-  <div class="card-header">Batches Page</div>
-  <div class="card-body">
-      
-      <form action="{{ url('batches') }}" method="post">
-        {!! csrf_field() !!}
-        <label>Batch Name</label></br>
-        <input type="text" name="name" id="name" class="form-control"></br>
-        <label>Course Name</label></br>
-        <input type="text" name="course_id" id="course_id" class="form-control"></br>
-        <label>Start Date</label></br>
-        <input type="text" name="start_date" id="start_date" class="form-control"></br>
-        <input type="submit" value="Save" class="btn btn-success"></br>
-    </form>
-   
-  </div>
-</div>
- 
+
+    <div class="card">
+        <div class="card-header">Batches Page</div>
+        <div class="card-body">
+
+            <form action="{{ url('batches') }}" method="post">
+                {!! csrf_field() !!}
+                <label>Batch Name</label></br>
+                <input type="text" name="name" id="name" class="form-control"></br>
+
+                <label>Course</label></br>
+                <select name="course_id" id="course_id" class="form-control">
+                    @foreach ($courses as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                    @endforeach
+                </select>
+
+
+                <label>Start Date</label></br>
+                <input type="text" name="start_date" id="start_date" class="form-control"></br>
+                <input type="submit" value="Save" class="btn btn-success"></br>
+            </form>
+
+        </div>
+    </div>
+
 @stop
