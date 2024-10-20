2975d27e801b0b367351cb6783c391bc798e283c
asiripr <asiripramodaya2000@gmail.com>
2024-10-20 15:26:08 +0530

    completed the payment part

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\BatchController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +18,4 @@ Route :: resource('/courses', CourseController :: class);
 Route :: resource('/batches', BatchController :: class);
 Route :: resource('/enrollments', EnrollmentController :: class);
 Route :: resource('/payments', PaymentController :: class);
+Route :: get('/report/report1/{pid}', [ReportController :: class, 'report1']);
